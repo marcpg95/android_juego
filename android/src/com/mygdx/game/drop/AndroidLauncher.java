31b3fc97b10b1ad3bca156bb2a34524b90b51c65
Marc <marcpg95@gmail.com>
2020-01-14 17:03:58 +0100

    juego droplet

---
+++ android/src/com/mygdx/game/drop/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.game.drop;
+
+
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new MyDropGame(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/game/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.drop.desktop;
+
+
+import com.mygdx.game.drop.MyDropGame;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new MyDropGame(), config);
+	}
+}
